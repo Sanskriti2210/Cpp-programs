b3df50644e50c25c06d236e0d91103784cde3ed1
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-01 15:25:01 +0530

    Solid rectangle using for loop.cpp

---
+++ Solid rectangle using for loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i=0;i<3;i=i+1)
+    {
+    for(int j=0;j<5;j=j+1){
+       cout<<"*";
+    }
+  cout<<endl;
+    }
+}
+
+Output:
+*****
+*****
+*****
