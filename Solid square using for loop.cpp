ffc5ae9ca8283dc88239fa557577fdc488d042af
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-01 15:31:48 +0530

    Create Solid square using for loop.cpp

---
+++ Solid square using for loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"Enter the number of n:";
+  cin>>n;
+  for(int i=0;i<n;i=i+1)
+    {
+    for(int j=0;j<n;j=j+1){
+       cout<<"* ";
+    }
+  cout<<endl;
+    }
+}
+
+Output:
+Enter the number of n:6
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+ 
