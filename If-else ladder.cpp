4b73347373f92135e85e1b353f37c6de1b6a7fbc
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-01 14:23:38 +0530

    Create If-else ladder.cpp

---
+++ If-else ladder.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int marks;
+  cout<<"Enter the marks:";
+  cin>>marks;
+
+   if(marks>=90){
+     cout<<"A grade";
+   }
+  else if(marks>=80){
+    cout<<"B grade";
+  }
+  else if(marks>=70){
+    cout<<"C grade";
+  }
+  else if(marks>=40){
+    cout<<"D grade";
+  }
+  else if(marks<40){
+     cout<<"F grade";
+  }
+  }
