1cf2efd58672c02b07b3ff3105594b4f7bf47a4b
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-01 14:14:24 +0530

    Create Relational operator.cpp

---
+++ Relational operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int a=25;
+  int b=22;
+
+  cout << (a<b)  << endl;
+  cout << (a>b)  << endl;
+  cout << (a<=b) << endl;
+  cout << (a>=b) << endl;
+  cout << (a!=b) << endl;
+  cout << (a==b) << endl;  
+  }
