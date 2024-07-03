adeb431eff7a2179d9eb03e31a730fefc8edeac7
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 01:04:13 +0530

    Inverted numeric half pyramid.cpp

---
+++ Inverted numeric half pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+  cin>>n;
+
+  for(int i=0;i<n;i=i+1){
+    for(int j=0;j<n-i;j=j+1){
+      cout<<j+1;
+    }
+    
+    cout<<endl;
+      }
+}
+
+Output:
+4
+1234
+123
+12
+1
+ 
