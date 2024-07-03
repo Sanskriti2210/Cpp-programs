6b9373aa93017dec144ef0abe49a0c7b3d32fef9
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 01:05:59 +0530

    Numeric half pyramid.cpp

---
+++ Numeric half pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+  cin>>n;
+
+  for(int i=0;i<n;i=i+1){
+    for(int j=0;j<i+1;j=j+1){
+      cout<<j+1;
+    }
+    
+    cout<<endl;
+      }
+}
+      
+Output:
+5
+1
+12
+123
+1234
+12345
+ 
